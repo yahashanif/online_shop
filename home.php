6827480c55aec4de9c5dd06bfc806829254c9298
yahashanif <hanifauliasabriii@gmail.com>
2022-01-24 18:05:04 +0700

    hgjj

--- home.php
+++ home.php
@@ -1,7 +1,7 @@
 <div class="container4">
 	<div class="grid">
 		<div class="dh12">
-		<img src="image/home.png" width="100%" height="600px">
+		<img src="image/home.png" width="100%" >
 		</div>
 	</div>
 </div>
