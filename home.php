277fc53721f0cf48cf36ccf1612784557c93df9f
yahashanif <hanifauliasabriii@gmail.com>
2022-01-24 17:56:44 +0700

    adssadsad

--- home.php
+++ home.php
@@ -1,7 +1,7 @@
 <div class="container4">
 	<div class="grid">
 		<div class="dh12">
-		<img src="image/home.png" width="100%">
+		<img src="image/home.png" width="100%" height="600px">
 		</div>
 	</div>
 </div>
